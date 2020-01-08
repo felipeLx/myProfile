a4a99137d6d20990812c9c39f339eb02daa6adbd
felipeLx <felipealvesfinanceiro@gmail.com>
2020-01-08 19:57:09 -0300

    add pics

--- src/components/Profile/Desktop/Desktop.js
+++ src/components/Profile/Desktop/Desktop.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
 import classes from './Desktop.module.css';
+import Photos from './Photos';
 
 const Desktop = props => {
     return(
         <div className={classes.Desktop}>
-            <p>photos</p>
+            <Photos />
         </div>
     );
 
--- src/components/Profile/Desktop/Photos.js
+++ src/components/Profile/Desktop/Photos.js
@@ -1,47 +1,40 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import ImageGallery from 'react-image-gallery';
+import React, { Component } from 'react';
 
-const regex = /\["(https:\/\/lh3\.googleusercontent\.com\/[a-zA-Z0-9\-_]*)"/g;
+import ReactBnbGallery from 'react-bnb-gallery'
 
+const photos = [{
+  photo: "https://photos.app.goo.gl/VuiXsVV4UPHdmJ7u7",
+  thumbnail:"https://photos.app.goo.gl/VuiXsVV4UPHdmJ7u7"
+}, {
+  photo: "https://photos.app.goo.gl/dTxGMLBG51MMUR1C6",
+  thumbnail: "https://photos.app.goo.gl/dTxGMLBG51MMUR1C6"
+}, {
+  photo: "https://photos.app.goo.gl/wyEf55BzJnedEydj9",
+  thumbnail: "https://photos.app.goo.gl/wyEf55BzJnedEydj9"
+},{
+  photo: "https://photos.app.goo.gl/jh64pE1yqDND5L3H6",
+  thumbnail: "https://photos.app.goo.gl/jh64pE1yqDND5L3H6"
+},{
+  photo: "https://photos.app.goo.gl/TGQU4r7c2hJtxRQ78",
+  thumbnail: "https://photos.app.goo.gl/TGQU4r7c2hJtxRQ78"
+}];
 
-const ImagesGallery = () => {
-    const [images, setImages] = useState(null);
+class Photos extends Component {
+  constructor() {
+    super(...arguments);
+    this.state = { galleryOpened: true };
+  }
 
-    function extractPhotos(content) {
-        const links = new Set();
-         let match;
-         while (match = regex.exec(content)) {
-           links.add(match[1])
-         } 
-         return Array.from(links);
-       }
+  render () {
+    return (
+      <div>
+        <ReactBnbGallery
+          show={this.state.galleryOpened}
+          photos={photos}
+           />
+      </div>
+    );
+  }
+}
 
-       async function getAlbum() {
-        try {
-          const response = await axios.get(`https://photos.app.goo.gl/8rShUULHWw9gGwXg6`);
-          return extractPhotos(response.data);
-        }
-        catch(e) {
-          return e.message;
-        }
-      }
-
-    useEffect(() => {
-        const response = await axios.get(
-            'https://google-photos-album-demo.glitch.me/YOUR_ALBUM_ID'
-          )
-          if (response && response.data && response.data.length > 0) {
-            this.setState({
-              images: response.data.map((url: string) => ({
-                original: `${url}=w1024`,
-                thumbnail: `${url}=w100`,
-              })),
-            })
-          }
-        }
-
-    return();
-};
-
-export default ImageGallery;
+export default Photos;
