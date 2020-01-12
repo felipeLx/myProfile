117862abfdbf8834aed73d6528bbcae7046318d0
felipeLx <felipealvesfinanceiro@gmail.com>
2020-01-12 00:49:01 -0300

    test css in map area

--- src/components/MyExperience/Map/Map.js
+++ src/components/MyExperience/Map/Map.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
 import classes from './Map.module.css';
-import map from '../../..assets/images/myexp.png';
+import map from '../../../assets/images/myexp.png';
 
 const Map = () => {
-    const openRioExperience = () => {
+    const openRio = () => {
         console.log("RJ clicked");
     }
     
@@ -23,10 +23,12 @@ const Map = () => {
     return (
         <div className={classes.Map}>
             <map name="map">
-                <area alt="rio" shape="rect" coords="460,789,493,816" onClick="alert('square');return false" />
-                <area alt="floripa" shape="rect" coords="403,878,430,901" onClick={openFloripa} />
-                <area alt="spain" shape="rect" coords="798,113,830,140" onClick={openSpain} />
-                <area alt="lisbon" shape="rect" coords="723,134,757,160" onClick={openLisbon} />
+                <div className={classes.Rio} >
+                <area alt="rio" shape="rect" coords="460,789,493,816" onClick={openRio} />
+                <area className={classes.Floripa} alt="floripa" shape="rect" coords="403,878,430,901" onClick={openFloripa} />
+                <area className={classes.Spain} alt="spain" shape="rect" coords="798,113,830,140" onClick={openSpain} />
+                <area className={classes.Lisbon} alt="lisbon" shape="rect" coords="723,134,757,160" onClick={openLisbon} />
+                </div>
             </map>
             <img 
                 src={map} 
