fca9a28ae333b67df7176e1801c327da533b6853
felipeLx <felipealvesfinanceiro@gmail.com>
2020-01-12 12:00:15 -0300

    erro map start

--- src/components/MyExperience/Map/Map.js
+++ src/components/MyExperience/Map/Map.js
@@ -3,9 +3,10 @@ import React from 'react';
 import classes from './Map.module.css';
 import map from '../../../assets/images/myexp.png';
 import Rio from './Rio/Rio';
+import Floripa from './Floripa/Floripa';
 
 const Map = () => {
-    const openRio = () => new Object.bind(<Rio />);
+    const openRio = () => new Object.call(<Rio />);
     
     const openFloripa = () => {
         console.log("SC clicked");
@@ -23,8 +24,8 @@ const Map = () => {
         <div className={classes.Map}>
             <map name="map">
                 <div className={classes.Area} >
-                <area alt="rio" shape="rect" coords="460,789,493,816" onClick={openRio} />
-                <area alt="floripa" shape="rect" coords="403,878,430,901" onClick={openFloripa} />
+                <area alt="rio" shape="rect" coords="460,789,493,816" onClick={<Rio />} />
+                <area alt="floripa" shape="rect" coords="403,878,430,901" onClick={<Floripa />} />
                 <area alt="spain" shape="rect" coords="798,113,830,140" onClick={openSpain} />
                 <area alt="lisbon" shape="rect" coords="723,134,757,160" onClick={openLisbon} />
                 </div>
